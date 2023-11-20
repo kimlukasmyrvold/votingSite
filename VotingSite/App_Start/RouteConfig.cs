7f9901e31476930e3d6d53e295beb204f5811877
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2023-11-20 10:07:03 +0100

    Removed partier page

--- VotingSite/App_Start/RouteConfig.cs
+++ VotingSite/App_Start/RouteConfig.cs
@@ -15,7 +15,6 @@ namespace VotingSite
             routes.EnableFriendlyUrls(settings);
 
             routes.MapPageRoute("KommunerRoute", "Kommuner", "~/Pages/Kommuner/Default.aspx");
-            routes.MapPageRoute("PartierRoute", "Partier", "~/Pages/Partier/Default.aspx");
             routes.MapPageRoute("PersonerRoute", "Personer", "~/Pages/Personer/Default.aspx");
             routes.MapPageRoute("PrivacyRoute", "Privacy", "~/Pages/Privacy/Default.aspx");
         }

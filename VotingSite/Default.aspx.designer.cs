54e7d8cdbdec0af8f92d733c444fdee20955257d
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2023-11-27 16:55:42 +0100

    Removed unnecessary element for displaying current year

--- VotingSite/Default.aspx.designer.cs
+++ VotingSite/Default.aspx.designer.cs
@@ -15,15 +15,6 @@ namespace VotingSite
     {
 
         /// <summary>
-        /// year control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl year;
-
-        /// <summary>
         /// partierContainer control.
         /// </summary>
         /// <remarks>
